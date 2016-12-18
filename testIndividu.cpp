68f037789afe39b86aa7e7209b0da76f7c215908
romainTeule <romteule@gmail.com>
2016-12-18 12:15:39 +0100

    Add files via upload
    Test des constructeurs et setters de etudiant et individu

---
+++ testIndividu.cpp
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+#include "Individu.h"
+
+TEST_CASE ("Le constructeur de Individu marche", "Individu")
+{
+	Individu individu("Jaures", "Jean");
+	
+	REQUIRE(individu.getNom() =="Jaures");
+	REQUIRE(individu.getPrenom() =="Jean");
+}
+
+TEST_CASE("Les setters marchent", "Individu")
+{
+	Individu individu("Jaures", "Jean");
+	
+	REQUIRE(individu.getNom() =="Jaures");
+	REQUIRE(individu.getPrenom() =="Jean");
+	
+	SECTION(" Changer le nom marche ")
+	{
+		individu.setNom("Francois");
+		REQUIRE(individu.getNom() =="Francois");
+	}
+	SECTION("Changer le prénom marche ")
+	{
+		individu.setPrenom("Claude");
+		REQUIRE(individu.getPrenom()=="Claude");
+	}
+}
