30a758a038b09ce61fdf9c0936168d5b3f5e6304
romainTeule <romteule@gmail.com>
2016-12-18 14:26:18 +0100

    Add files via upload
    Remplacer les objets professeur, salles et filière contenus dans la classe par des pointeurs

--- Cours.cpp
+++ Cours.cpp
@@ -1,46 +1,57 @@
 #include "Cours.h"
 
-Cours::Cours(const Filiere& fil, const Professeur& prof, const Salle& sal, int hor) :
-	d_filiere{fil}, d_professeur{prof}, d_salle{sal}, d_horraire{hor}
+Cours::Cours(Filiere* fil,Professeur* prof,Salle* sal, int hor, int duree) :
+	d_filiere{fil}, d_professeur{prof}, d_salle{sal}, d_horraireDebut{hor}, d_duree{duree}
 {
 }
 
 Filiere Cours::getFiliere() const
 {
-	return d_filiere;
+	return *d_filiere;
 }
 
-void Cours::setFiliere(const Filiere& fil)
+void Cours::setFiliere(Filiere* fil)
 {
 	d_filiere = fil;
 }
 
 Professeur Cours::getProfesseur() const
 {
-	return d_professeur;
+	return *d_professeur;
 }
 
-void Cours::setProfesseur(const Professeur& prof)
+void Cours::setProfesseur(Professeur* prof)
 {
 	d_professeur = prof;
 }
 
 Salle Cours::getSalle() const
 {
-	return d_salle;
+	return *d_salle;
 }
 
-void Cours::setSalle(const Salle& sal)
+void Cours::setSalle(Salle* sal)
 {
 	d_salle = sal;
 }
 
 int Cours::getHorraire() const
 {
-	return d_horraire;
+	return d_horraireDebut;
 }
 
 void Cours::setHorraire(const int& hor)
 {
-	d_horraire = hor;
+	d_horraireDebut = hor;
 }
+
+int Cours::getDuree() const
+{
+	return d_duree;
+}
+
+void Cours::setDuree(const int& duree) 
+{
+	d_duree=duree;
+}
+
--- Cours.h
+++ Cours.h
@@ -8,22 +8,24 @@
 class Cours
 {
 	public:
-		Cours(const Filiere& fil, const Professeur& prof, const Salle& sal, int hor);
+		Cours(Filiere* fil,Professeur* prof,Salle* sal, int hor, int duree=2);
 		
 		Filiere getFiliere() const;
-		void setFiliere(const Filiere& fil);
+		void setFiliere(Filiere* fil);
 		Professeur getProfesseur() const;
-		void setProfesseur(const Professeur& prof);
+		void setProfesseur(Professeur* prof);
 		Salle getSalle() const;
-		void setSalle(const Salle& sal);
+		void setSalle(Salle* sal);
 		int getHorraire() const;
 		void setHorraire(const int& hor);
-				
+		int getDuree() const;
+		void setDuree(const int& duree);		
 	private:
-		Filiere d_filiere;
-		Professeur d_professeur;
-		Salle d_salle;
-		int d_horraire;
+		Filiere* d_filiere;
+		Professeur* d_professeur;
+		Salle* d_salle;
+		int d_horraireDebut;
+		int d_duree;
 };
 
 #endif
