1aadaaed0d8ce9d9f4fee21ae460451e844cb8ba
allefou <allefou@hotmail.fr>
2017-01-08 10:07:25 +0100

    test Journee fini

--- TestUnitaire/testJournee.cpp
+++ TestUnitaire/testJournee.cpp
@@ -3,7 +3,6 @@
 
 TEST_CASE("Le constructeur de journee marche", "Journee")
 {
-	Journee journee();
 	vector<Cours*> coursJournee;
 	
 	for (int i=0; i<10; i++)
@@ -11,7 +10,38 @@ TEST_CASE("Le constructeur de journee marche", "Journee")
 		coursJournee.push_back(new Cours(new Professeur("Bibine","Renée","Quidditch"), new Salle("F402", "6"), 21401196, 21401196));
 	}
 	
-	journee.Journee(coursJournee);
+	Journee journee(coursJournee);
 	REQUIRE(journee.getCoursJournee()==journee);
 }
- 
+
+
+TEST_CASE("Les setter de journee marche", "Journee")
+{
+	vector<Cours*> coursJournee;
+	
+	for (int i=0; i<10; i++)
+	{
+		coursJournee.push_back(new Cours(new Professeur("Bibine","Renée","Quidditch"), new Salle("F402", "6"), 21401196, 21401196));
+	}
+	
+	Journee journee(coursJournee);
+	REQUIRE(journee.getCoursJournee()==journee);
+
+	SECTION("Changer la journee marche")
+	{
+		vector<Cours*> autreCoursJournee;
+		
+		for (int i=0; i<10; i++)
+		{
+			autreCoursJournee.push_back(new Cours(new Professeur("McGonagall","Minerva","Métamorphose"), new Salle("B600", "B"), 21401196, 21401196));
+		}
+		journee.setCoursJournee(autreCoursJournee);
+		REQUIRE(journee.getCoursJournee()==autreCoursJournee);
+	}
+	SECTION("Changer la journee marche")
+	{
+		Cours* autreCours = new Cours(new Professeur("McGonagall","Minerva","Métamorphose"), new Salle("B600", "B"), 21401196, 21401196);
+		journee.ajouterCours(autreCours, 0);
+		REQUIRE(journee.getCoursJournee()[0]==autreCours);
+	}
+}
