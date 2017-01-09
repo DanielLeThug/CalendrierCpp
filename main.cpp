6cc63e4d6bcf5709822b334d41455fe4f6f3cd26
teezul <jp.dubuc1106@gmail.com>
2017-01-09 15:52:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+#include "EmploiDuTemps.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-/*int main(int argc, char** argv) {
+int main(int argc, char** argv) {
+	
+	EmploiDuTemps EDT;
+	
+	EDT.loopFenetrePrincipale();
+	
 	return 0;
-}*/
+}
